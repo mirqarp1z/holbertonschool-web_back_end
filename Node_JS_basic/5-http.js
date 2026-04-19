abeffa7b48175de7c4c716c2f1d15781bf64d42d
mirqarp1z <orucaydin13@gmail.com>
2026-04-19 17:25:56 +0000

    task

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,39 @@
+const http = require('http');
+
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = args[0];
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer(async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  const { url } = req;
+
+  if (url === '/') {
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+    return;
+  }
+
+  if (url === '/students') {
+    res.statusCode = 200;
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(DATABASE);
+      res.end(students.join('\n'));
+    } catch (error) {
+      res.end(error.message);
+    }
+    return;
+  }
+  res.statusCode = 404;
+  res.end();
+});
+
+app.listen(port, hostname);
+
+module.exports = app;
